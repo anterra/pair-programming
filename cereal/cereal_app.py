d7f9dbdeaef605af3ff4bd67d9949eafa841ab83
Anterra Kennedy <anterrakennedy@gmail.com>
2020-09-17 12:13:11 -0700

    fixed cereal app:

--- cereal/cereal_app.py
+++ cereal/cereal_app.py
@@ -9,15 +9,15 @@ f.close()
 
 st.title("CEREAL WORLD")
 st.write("Find Your Dream Cereal")
-sugar = st.slider("Max Sugar", -1, 15)
-cals = st.slider("Max Calories", 50, 160)
-sodium = st.slider("Max Sodium", 0, 320)
+sugar = st.slider("Max Sugar", -1, 15, 15)
+cals = st.slider("Max Calories", 50, 160, 160)
+sodium = st.slider("Max Sodium", 0, 320, 320)
 protein = st.slider("Min Protein", 1, 6)
 fiber = st.slider("Min Fiber", 0, 14)
 potass = st.slider("Min Potassium", -1, 330)
 
 cereals = df.loc[(df["sugars"] <= sugar) & (df["calories"] <= cals) & (df["sodium"] <= sodium) &
-                 (df["protein"] >= protein) & (df["fiber"] >= fiber) & df["potass"] >= potass]
+                 (df["protein"] >= protein) & (df["fiber"] >= fiber) & (df["potass"] >= potass)]
 cereal_names = cereals["name"]
 your_cereal = np.random.choice(cereal_names)
 
